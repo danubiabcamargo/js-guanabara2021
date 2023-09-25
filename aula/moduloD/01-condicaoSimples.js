c8a6f8e9681359dd6327bbad1329547992bcdf28
Danubia Camargo <danubiabcamargo@gmail.com>
2023-09-25 17:27:16 -0300

    modD

---
+++ aula/moduloD/01-condicaoSimples.js
@@ -0,0 +1,32 @@
+/*
+  if se (condição){ criando um bloco
+caso essa condição seja verdade
+
+
+  }else senão { outro bloco
+caso essa condioção for falsa
+  }
+
+//tipos de Condição
+1 tipo de bloco
+
+if(condição){
+  condição simples
+}
+
+
+2 tipo de condição
+if(confição){
+
+}else{
+
+}
+*/
+
+let vel =65;
+console.log(`A Velocidade do seu carro é ${vel}Km/h`);
+if(vel>60){
+  console.log('Você ultrapssou a velocidade permitida. MULTADO!');
+}
+console.log('Dirija sempre usando cinto de segurança');
+
