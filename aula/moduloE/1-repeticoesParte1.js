7fce68ed5e2dbbcb2522020621c3b55de9561c70
Danubia Camargo <danubiabcamargo@gmail.com>
2023-10-02 16:49:41 -0300

    ex

--- aula/moduloE/1-repeticoesParte1.js
+++ aula/moduloE/1-repeticoesParte1.js
@@ -1,5 +1,5 @@
 let contador = 1;
-do{
+do{ //faz o bloco e testa depois, teste logico feito no final
   console.log(`Passo ${contador}`)
   contador ++    
 } while (contador <= 10)  // do faça isso tudo enquanto o contador for menor que 10
@@ -8,7 +8,7 @@ do{
 
 /*
 let contador = 1;
-while(contador <= 10){
+while(contador <= 10){ // teste logico feito no inicio
   console.log(`Passo ${contador}`)
   contador ++    //ele volta para contabilizar ex: c=1 volta c=2 para diminuit contagem do 6
 }
---
+++ aula/moduloE/2-repeticoesParte2.js
@@ -0,0 +1,20 @@
+//estrutura de repetição com variavel de controle 
+//passo a passou - inicialização, teste logico,se for vdd ele executa o bloco, volta e faz o incremento.... caso falso faz o caminho de encerramento...
+
+// for (inicio;teste;incremento){ }
+
+/*
+var c = 1
+while (c<=5){
+  console.log(c)
+  c++
+}
+
+*/
+//-----------------------------------------------
+for (var c = 1; c <= 5;c++ ){
+  console.log(c)
+}
+
+
+//modo depuração
